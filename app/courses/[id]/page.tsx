d6d5aa36584628bbb908198c7d1b49f944aa1d08
Bao Ngoc HUYNH <baongochuynh.hbn@gmail.com>
2026-09-11 17:11:58 +0200

    feat: add status not found

--- app/courses/[id]/page.tsx
+++ app/courses/[id]/page.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
 import {courses} from "@/data/courses";
 import type { Course } from "@/types/courses";
 import CourseDetailHeader from "@/features/courseDetails/components/CourseDetailHeader/CourseDetailHeader";
@@ -8,28 +11,26 @@ export default async function CourseDetailsPage({ params }: { params: { id: stri
     const { id } = await params;
     const idNumber = parseInt(id);
     const course: Course | undefined = courses.find((course: Course) => course.id === idNumber);
+    if(!course) {
+        notFound();
+    }
 
-    console.log(course);
     return (
         <div className="min-h-screen bg-slate-950 px-6 py-10">
             <div  className="mx-auto max-w-7xl">
 
-                <a href="/courses" className="mb-6 inline-block rounded bg-blue-600 px-4 py-2 text-white hover:bg-blue-700" > ← Back to Courses </a>
+                <Link href="/courses" className="mb-6 inline-block rounded bg-blue-600 px-4 py-2 text-white hover:bg-blue-700" > ← Back to Courses</Link>
 
-                {course ? (
-                    <div>
-                        <CourseDetailHeader course={course} />
-                        <div className="mt-8 grid grid-cols-1 gap-8 lg:grid-cols-[1fr_360px]"> 
-                            <CourseDetailMainContent course={course} /> 
-                            
-                            <aside className="lg:sticky lg:top-6 lg:self-start"> 
-                                <EnrollmentCard course={course} /> 
-                            </aside>
-                        </div>
+                <div>
+                    <CourseDetailHeader course={course} />
+                    <div className="mt-8 grid grid-cols-1 gap-8 lg:grid-cols-[1fr_360px]"> 
+                        <CourseDetailMainContent course={course} /> 
+                        
+                        <aside className="lg:sticky lg:top-6 lg:self-start"> 
+                            <EnrollmentCard course={course} /> 
+                        </aside>
                     </div>
-                ) : (
-                    <p className="text-slate-400">Course not found.</p>
-                )}
+                </div>
             </div>
         </div>
     )
